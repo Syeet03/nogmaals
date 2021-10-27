0d09849df2870a8fcc10580163fa75496dd8c5c7
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-10-27 09:52:41 +0200

    Oefening 13

---
+++ robotarm-python-2021-main/Oefening13.py
@@ -0,0 +1,27 @@
+from RobotArm import RobotArm
+# Let op: hier start het anders voor een random level:
+robotArm = RobotArm()
+robotArm.randomLevel(1,7)
+
+# Jouw python instructies zet je vanaf hier:
+loop = 1
+rounds = 0
+
+while loop == 1:
+    robotArm.grab()
+    scan = robotArm.scan()
+
+    if scan != "":
+        rounds += 1
+        for i in range(0,rounds):
+            robotArm.moveRight()
+            
+        robotArm.drop()
+
+        for i in range(0,rounds):
+            robotArm.moveLeft()
+    else:
+        loop = 0
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
