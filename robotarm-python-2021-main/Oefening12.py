45cd413e1cad54af8e94d8f6c7b304dff3c2a374
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-11-23 09:57:04 +0100

    Oefening 12

--- robotarm-python-2021-main/Oefening12.py
+++ robotarm-python-2021-main/Oefening12.py
@@ -3,24 +3,25 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
-for i in range(0,8):
+x = 0
+
+for i in range(9):
+    robotArm.grab()
+    scanResult = robotArm.scan()
+    
+    if scanResult != 'red':
+        robotArm.drop()
+        
+    else:
+        for i in range(9):
+            robotArm.moveRight()
+        robotArm.drop()
+        for y in range(9-x):
+            robotArm.moveLeft()
+
     robotArm.moveRight()
 
-for i in range(1,9):
-    robotArm.grab()
-    scan = robotArm.scan()
+    x+=1 
 
-    if scan == "red":
-        for c in range(0,i):
-            robotArm.moveRight()
-            
-        robotArm.drop() 
-
-        for c in range(0,i):
-            robotArm.moveLeft()  
-    else:
-        robotArm.drop()
-    robotArm.moveLeft()
-
-# Na jouw code wachten tot het sluiten van de window:
+# Na jouw code wachten tot het sluiten van de window: 
 robotArm.wait()
