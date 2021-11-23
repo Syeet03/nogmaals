9838ca018863f8fd0a2cc2e8875c4e0abde47f4f
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-11-23 09:51:15 +0100

    Oefening 10

--- robotarm-python-2021-main/Oefening10.py
+++ robotarm-python-2021-main/Oefening10.py
@@ -3,16 +3,19 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 10')
 
 # Jouw python instructies zet je vanaf hier:
-for i in range(5):
+for z in range(1,6):
     robotArm.grab()
 
-    for i in range(5):
+    for x in range(2,13-(z*2)):
         robotArm.moveRight()
-
+        
     robotArm.drop()
 
-    for i in range(4):
+    for y in range(9):
         robotArm.moveLeft()
 
-# Na jouw code wachten tot het sluiten van de window:
+    for i in range(z):
+        robotArm.moveRight()
+
+        # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
